f8052b253f26d37bfa74380d35580743526a32a2
Andrew Wang <Andrewruujin@gmail.com>
2022-08-21 22:11:11 -0400

    commit tree 19941b26282d081b637263568d77e8fdd9c2b5fa

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(name){
+    const x = function(y=name){
+        let driver =  y.slice(0,2);
+        return driver;
+    };
+    return x();
+    
+}
+function returnLastTwoDrivers(name){
+    const x = function(y = name){ 
+        let driver = y.slice(drivers.length-2,drivers.length)
+        return driver;
+    };
+    return x();
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(x){
+    const theFare = fare => fare * x;
+    return theFare;
+}
+
+function fareDoubler(x){
+    const doubleing = createFareMultiplier(2);
+    return doubleing(x);
+}
+
+function fareTripler(x){
+    const trip = createFareMultiplier(3);
+    return trip(x);
+}
+function selectDifferentDrivers(names,driver){
+    return driver(names);
+}
+
